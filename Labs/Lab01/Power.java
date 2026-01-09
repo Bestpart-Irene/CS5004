f6e9b135b2ea6ea0bd272dcf420a21a369bbf233
irene-the-best <xxiellan@gmail.com>
2026-01-09 00:20:14 -0800

    Adding the required scripts

---
+++ Labs/Lab01/Power.java
@@ -0,0 +1,12 @@
+public class Power {
+    public static void main(String[] args) {
+        long result = (long) Math.pow(2,31);
+        System.out.println(result);
+    }
+}
+
+/*
+ * integer is different: Python does not declare type, Java declears and it is long
+ * power calculation method: Python do directly, Java needs a Math module
+ * syntax: Java needs class for the main.
+ */
